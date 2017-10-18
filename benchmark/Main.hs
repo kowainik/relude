167a0c6d6fcad381ad8aa708d5dfa62829581b27
vrom911 <vrom911@gmail.com>
2017-10-19 00:27:20 +0300

    Add TextNub benchmarks (#21)

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,19 +1,31 @@
-import           Criterion.Main
-import           Data.Hashable
+{-# LANGUAGE ExplicitForAll      #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import           Control.DeepSeq    (NFData)
+import           Criterion.Main     (Benchmark, bench, bgroup, defaultMain, nf)
+import           Data.Hashable      (Hashable)
 import qualified Data.HashSet       as HSet
 import           Data.List          (group, head, nub, sort)
-import qualified Data.List.NonEmpty as S
+import qualified Data.List.NonEmpty as NonEmpty
 import qualified Data.Set           as Set
+import           Data.Text          (Text)
+import qualified Data.Text          as T
 
 import           List               (hashNub, ordNub)
 
 main :: IO ()
 main = defaultMain
-  [ bgroupList listOfSmall "small"
-  , bgroupList listOfBig "big"
+  [ bgroupList listOfSmall    "small"
+  , bgroupList listOfBig      "big"
+  , bgroupList (nStrings 'z') "small str"
+  , bgroupList (nStrings 'c') "big str"
   ]
 
-bgroupList :: (Int -> [Int]) -> String -> Benchmark
+bgroupList :: forall a .
+              (Ord a, Hashable a, NFData a)
+           => (Int -> [a])
+           -> String
+           -> Benchmark
 bgroupList f name = bgroup name $
   map ($ f)
   [ bgroupNubAll 100
@@ -23,47 +35,53 @@ bgroupList f name = bgroup name $
   , bgroupNubHugeList 500000
   , bgroupNubHugeList 1000000
   ]
-
-bgroupNubAll :: Int -> (Int -> [Int]) -> Benchmark
-bgroupNubAll = bgroupNub True
-
-bgroupNubHugeList :: Int -> (Int -> [Int]) -> Benchmark
-bgroupNubHugeList = bgroupNub False
-
-bgroupNub :: Bool -> Int -> (Int -> [Int]) -> Benchmark
-bgroupNub isNub n listOf =
-  bgroup (show n) nubBenchs
  where
-  listN :: [Int]
-  listN = listOf n
+  bgroupNubAll :: Int -> (Int -> [a]) -> Benchmark
+  bgroupNubAll = bgroupNub True
 
-  nubBenchs :: [Benchmark]
-  nubBenchs =
-    (if isNub
-    then (:) (bench "nub" $ nf nub listN)
-    else id)
-    [ bench "ordNub"   $ nf ordNub     listN
-    , bench "hashNub"  $ nf hashNub    listN
-    , bench "set"      $ nf setNub     listN
-    , bench "hashSet"  $ nf hashSetNub listN
-    , bench "sort"     $ nf groupSort  listN
-    , bench "safeSort" $ nf safeSort   listN
-    ]
+  bgroupNubHugeList :: Int -> (Int -> [a]) -> Benchmark
+  bgroupNubHugeList = bgroupNub False
 
-setNub :: Ord a => [a] -> [a]
-setNub = Set.toList . Set.fromList
+  bgroupNub :: Bool -> Int -> (Int -> [a]) -> Benchmark
+  bgroupNub isNub n listOf =
+    bgroup (show n) nubBenchs
+   where
+    listN :: [a]
+    listN = listOf n
 
-hashSetNub :: (Eq a, Hashable a) => [a] -> [a]
-hashSetNub = HSet.toList . HSet.fromList
+    nubBenchs :: [Benchmark]
+    nubBenchs =
+      (if isNub
+      then (:) (bench "nub" $ nf nub listN)
+      else id)
+      [ bench "ordNub"   $ nf ordNub     (listN :: [a])
+      , bench "hashNub"  $ nf hashNub    (listN :: [a])
+      , bench "set"      $ nf setNub     (listN :: [a])
+      , bench "hashSet"  $ nf hashSetNub (listN :: [a])
+      , bench "sort"     $ nf groupSort  (listN :: [a])
+      , bench "safeSort" $ nf safeSort   (listN :: [a])
+      ]
 
-groupSort :: Ord a => [a] -> [a]
-groupSort = map head . group . sort
+  setNub :: [a] -> [a]
+  setNub = Set.toList . Set.fromList
 
-safeSort :: Ord a => [a] -> [a]
-safeSort = map S.head . S.group . sort
+  hashSetNub :: [a] -> [a]
+  hashSetNub = HSet.toList . HSet.fromList
+
+  groupSort :: [a] -> [a]
+  groupSort = map head . group . sort
+
+  safeSort :: [a] -> [a]
+  safeSort = map NonEmpty.head . NonEmpty.group . sort
 
 listOfSmall :: Int -> [Int]
 listOfSmall n = let part = n `div` 100 in concat $ replicate part [1..100]
 
 listOfBig :: Int -> [Int]
 listOfBig n = let part = n `div` 2 in [1..part] ++ [1..part]
+
+allStrings :: Char -> [String]
+allStrings ch =  [ c : s | s <- "" : allStrings ch, c <- ['a'..ch] ]
+
+nStrings :: Char -> Int -> [Text]
+nStrings ch n = take n $ map T.pack $ allStrings ch
