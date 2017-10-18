1140d3d72861826b4348bc8a7fe45de4d3e6ad68
vrom911 <vrom911@gmail.com>
2017-10-19 02:43:00 +0300

    Add SuperComposition benchmarks (#21)

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -5,13 +5,14 @@ import           Control.DeepSeq    (NFData)
 import           Criterion.Main     (Benchmark, bench, bgroup, defaultMain, nf)
 import           Data.Hashable      (Hashable)
 import qualified Data.HashSet       as HSet
-import           Data.List          (group, head, nub, sort)
+import           Data.List          (group, head, nub, sort, zip5)
 import qualified Data.List.NonEmpty as NonEmpty
 import qualified Data.Set           as Set
 import           Data.Text          (Text)
 import qualified Data.Text          as T
 
 import           List               (hashNub, ordNub)
+import           VarArg             ((...))
 
 main :: IO ()
 main = defaultMain
@@ -19,6 +20,7 @@ main = defaultMain
   , bgroupList listOfBig      "big"
   , bgroupList (nStrings 'z') "small str"
   , bgroupList (nStrings 'c') "big str"
+  , bgroupSuperComposition
   ]
 
 bgroupList :: forall a .
@@ -85,3 +87,29 @@ allStrings ch =  [ c : s | s <- "" : allStrings ch, c <- ['a'..ch] ]
 
 nStrings :: Char -> Int -> [Text]
 nStrings ch n = take n $ map T.pack $ allStrings ch
+
+bgroupSuperComposition :: Benchmark
+bgroupSuperComposition = bgroup "(...)"
+  [ bgroup "show+" [ bench "super" $ nf (show ... (+) 1) (2 :: Int)
+                   , bench "norm"  $ nf (show . ((+) 1)) (2 :: Int)
+                   ]
+  , bgroup "show"  [ bench "super" $ nf (show ...) (5 :: Int)
+                   , bench "norm"  $ nf show       (5 :: Int)
+                   ]
+  , bgroup "zip5"  [ bench "super" $ nf ((null ... zip5) [()] [()] [()] []) [()]
+                   , bench "norm"  $ nf (null . (zip5 [()] [()] [()] []) )  [()]
+                   ]
+  , bgroup "10x"   [ bench "super" $ nf super10 [()]
+                   , bench "norm"  $ nf norm10  [()]
+                   ]
+  ]
+ where
+  super10 = null
+        ... (: []) ... head ... pure ... head
+        ... (: [(), (), (), ()]) ... head ... (: []) ... head
+        ... (: [()]) ... head ... (: [(), ()]) ... head
+
+  norm10 = null
+         . (: []) . head . pure . head
+         . (: [(), (), (), ()]) . head . (: []) . head
+         . (: [()]) . head . (: [(), ()]) . head
