6ae961ff1b5ffc07f7f048a6ead85a192d5ff8eb
vrom911 <vrom911@gmail.com>
2017-12-22 01:27:58 +0300

    [#100] Add bug

--- src/Universum/Exception.hs
+++ src/Universum/Exception.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE CPP                   #-}
+{-# LANGUAGE ImplicitParams        #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE Safe                  #-}
 
@@ -7,6 +8,10 @@
 
 module Universum.Exception
        ( module Control.Exception.Safe
+#if ( __GLASGOW_HASKELL__ >= 800 )
+       , Bug (..)
+       , bug
+#endif
        , note
        ) where
 
@@ -18,6 +23,25 @@ import Control.Exception.Safe (Exception, MonadCatch, MonadMask (..), MonadThrow
 import Control.Applicative (Applicative (pure))
 import Control.Monad.Except (MonadError, throwError)
 import Data.Maybe (Maybe, maybe)
+
+#if ( __GLASGOW_HASKELL__ >= 800 )
+import GHC.Show (Show)
+import GHC.Stack (CallStack, HasCallStack)
+
+import qualified Control.Exception.Safe as Safe (impureThrow, toException)
+
+-- | Type that represents exceptions used in cases when a particular codepath
+-- is not meant to be ever executed, but happens to be executed anyway.
+data Bug = Bug SomeException CallStack
+    deriving (Show)
+
+instance Exception Bug
+
+-- | Generate a pure value which, when forced, will synchronously
+-- throw the exception wrapped into 'Bug' data type.
+bug :: (HasCallStack, Exception e) => e -> a
+bug e = Safe.impureThrow (Bug (Safe.toException e) ?callStack)
+#endif
 
 -- To suppress redundant applicative constraint warning on GHC 8.0
 -- | Throws error for 'Maybe' if 'Data.Maybe.Nothing' is given.
