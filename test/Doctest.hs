740310f44084b310480fa1c02714be88df8cddbb
Dmitry Kovanikov <kovanikov@gmail.com>
2018-01-17 23:07:49 +0300

    [#53] Disable doctest for GHC < 8.2.2

--- test/Doctest.hs
+++ test/Doctest.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE CPP #-}
+
 module Main (main) where
+
+#if MIN_VERSION_base(4,10,1)
 
 import System.FilePath.Glob (glob)
 import Test.DocTest (doctest)
@@ -7,3 +11,10 @@ main :: IO ()
 main = do
     sourceFiles <- glob "src/**/*.hs"
     doctest $ "-XNoImplicitPrelude" : sourceFiles
+
+#else
+
+main :: IO ()
+main = return ()
+
+#endif
