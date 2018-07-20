4cf536fb0153ded63fd72b61f325ec4ef02dce72
Dmitry Kovanikov <kovanikov@gmail.com>
2018-07-20 21:52:28 +0800

    [#14] Introduce *.Extra modules

---
+++ src/Universum/Extra/Enum.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
+
+module Universum.Extra.Enum
+       ( universe
+       , next
+       , safeToEnum
+       ) where
+
+import Universum
+
+-- $setup
+-- >>> :set -XTypeApplications
+
+{- | Returns all values of some 'Bounded' 'Enum' in ascending order.
+
+>>> data TrafficLight = Red | Blue | Green deriving (Show, Enum, Bounded)
+>>> universe :: [TrafficLight]
+[Red,Blue,Green]
+>>> universe :: [Bool]
+[False,True]
+-}
+universe :: (Bounded a, Enum a) => [a]
+universe = [minBound .. maxBound]
+
+{- | Like 'succ', but doesn't fail on 'maxBound'. Instead it returns 'minBound'.
+
+>>> next False
+True
+>>> next True
+False
+>>> succ True
+*** Exception: Prelude.Enum.Bool.succ: bad argument
+
+-}
+next  :: (Eq a, Bounded a, Enum a) => a -> a
+next e
+    | e == maxBound = minBound
+    | otherwise     = succ e
+
+{- | Returns 'Nothing' if given 'Int' outside range.
+
+>>> safeToEnum @Bool 0
+Just False
+>>> safeToEnum @Bool 1
+Just True
+>>> safeToEnum @Bool 2
+Nothing
+>>> safeToEnum @Bool (-1)
+Nothing
+-}
+safeToEnum :: forall a . (Bounded a, Enum a) => Int -> Maybe a
+safeToEnum i = guard (fromEnum @a minBound <= i && i <= fromEnum @a maxBound) *> Just (toEnum i)
