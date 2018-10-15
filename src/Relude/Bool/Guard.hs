7a106010e351b3d1798afc0f6c763b2e6c46d1c9
Aleksey Pirogov <astynax@users.noreply.github.com>
2018-10-15 05:00:53 +0300

    Fix doctests in `Relude.Bool.Guard` (#105)
    * Fix doctests in `Relude.Bool.Guard`
    
    * notes

--- src/Relude/Bool/Guard.hs
+++ src/Relude/Bool/Guard.hs
@@ -24,6 +24,7 @@ import Relude.Monad (Monad, return, MonadPlus, (>>=))
 -- $setup
 -- >>> import Relude.Applicative (pure)
 -- >>> import Relude.Bool.Reexport (Bool (..))
+-- >>> import Relude.Debug (error)
 -- >>> import Relude.Function (($))
 -- >>> import Relude.Monad (Maybe (..))
 -- >>> import Relude.Print (putTextLn)
@@ -78,7 +79,7 @@ guardM f = f >>= guard
 
 -- | Monadic version of 'Data.Bool.(&&)' operator.
 --
--- >>> Just False &&^ undefined
+-- >>> Just False &&^ error "Shouldn't be evaluated"
 -- Just False
 (&&^) :: Monad m => m Bool -> m Bool -> m Bool
 (&&^) e1 e2 = ifM e1 e2 (return False)
@@ -86,7 +87,7 @@ guardM f = f >>= guard
 
 -- | Monadic version of 'Data.Bool.(||)' operator.
 --
--- >>> Just True ||^ undefined
+-- >>> Just True ||^ error "Shouldn't be evaluated"
 -- Just True
 (||^) :: Monad m => m Bool -> m Bool -> m Bool
 (||^) e1 e2 = ifM e1 (return True) e2
