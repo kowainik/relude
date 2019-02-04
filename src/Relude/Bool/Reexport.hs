c7fa5c78c0c5531256abf55bdc596f9e3c138f13
Dmitry Kovanikov <kovanikov@gmail.com>
2019-02-04 13:01:11 +0800

    Update stack.yaml

--- src/Relude/Bool/Reexport.hs
+++ src/Relude/Bool/Reexport.hs
@@ -1,6 +1,6 @@
 {- |
 Copyright:  (c) 2016 Stephen Diehl
-            (c) 20016-2018 Serokell
+            (c) 2016-2018 Serokell
             (c) 2018-2019 Kowainik
 License:    MIT
 Maintainer: Kowainik <xrom.xkov@gmail.com>
