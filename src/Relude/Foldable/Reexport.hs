046e7725d0f8ee68d46453d7a0c07f73f5eea507
Taylor Fausak <taylor@fausak.me>
2019-10-02 22:09:00 -0400

    Reexport foldMap' from new enough versions

--- src/Relude/Foldable/Reexport.hs
+++ src/Relude/Foldable/Reexport.hs
@@ -20,6 +20,9 @@ module Relude.Foldable.Reexport
 import Data.Foldable (Foldable (fold, foldMap, foldl', foldr, length, null, toList), all, and, any,
                       asum, concat, concatMap, find, foldlM, forM_, for_, mapM_, or, sequenceA_,
                       sequence_, traverse_)
+#if MIN_VERSION_base(4,13,0)
+import Data.Foldable (foldMap')
+#endif
 import Data.Traversable (Traversable (..), forM, mapAccumL, mapAccumR)
 #if MIN_VERSION_base(4,10,0)
 import Data.Bifoldable (Bifoldable (..), biList, biall, biand, biany, biasum, bielem, bifind,
