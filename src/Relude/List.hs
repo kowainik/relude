9b7b2734d09ab02dc36afd6aa2c7c22cca75d9b7
Flavio Corpa <flaviocorpa@gmail.com>
2019-10-07 18:23:59 +0200

    Implement safe `at` (!!) function (#204)
    * Implement safe `at` (!!) function
    
    * Add missing operators 🤦🏼‍♂️
    
    * Import correct comparator operators...
    
    * Apply feedback! 😁
    
    * Add entry to changelog!
    
    * Compile locally and fix things (finally)
    
    * Apply feedback, round 2!

--- src/Relude/List.hs
+++ src/Relude/List.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE Safe #-}
+{-# LANGUAGE Trustworthy #-}
 
 {- |
 Copyright:  (c) 2016 Stephen Diehl
@@ -13,7 +13,44 @@ Utility functions to work with lists.
 module Relude.List
        ( module Relude.List.NonEmpty
        , module Relude.List.Reexport
+       , (!!?)
        ) where
 
+
+import Relude.Base ((<))
+import Relude.Bool (otherwise)
 import Relude.List.NonEmpty
 import Relude.List.Reexport
+import Relude.Monad (Maybe (..))
+import Relude.Numeric (Int, (-))
+
+
+-- $setup
+-- >>> import Relude
+
+{- | Safer version of 'Relude.Unsafe.!!', returns a Maybe.
+get element from list using index value starting from `0`.
+
+>>> [] !!? 0
+Nothing
+
+>>> ["a", "b", "c"] !!? 3
+Nothing
+
+>>> [1, 2, 3] !!? (-1)
+Nothing
+
+>>> ["a", "b", "c"] !!? 2
+Just "c"
+-}
+infix 9 !!?
+(!!?) :: [a] -> Int -> Maybe a
+(!!?) xs i
+    | i < 0     = Nothing
+    | otherwise = go i xs
+  where
+    go :: Int -> [a] -> Maybe a
+    go 0 (x:_)  = Just x
+    go j (_:ys) = go (j - 1) ys
+    go _ []     = Nothing
+{-# INLINE (!!?) #-}
