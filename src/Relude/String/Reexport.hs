4120218aed18b34ab2353ee0d0e396fff55d0b87
Dmitrii Kovanikov <kovanikov@gmail.com>
2019-10-16 19:29:51 +0300

    [#171] Specify custom type errors for Text functions

--- src/Relude/String/Reexport.hs
+++ src/Relude/String/Reexport.hs
@@ -1,3 +1,11 @@
+{-# LANGUAGE ConstraintKinds      #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE KindSignatures       #-}
+{-# LANGUAGE Trustworthy          #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 {- |
 Copyright:  (c) 2016 Stephen Diehl
             (c) 2016-2018 Serokell
@@ -12,12 +20,16 @@ and 'ShortByteString' types.
 module Relude.String.Reexport
        ( -- * String
          module Data.String
+       , module Text.Read
 
          -- * Text
-       , module Data.Text
+       , Text
+       , lines
+       , unlines
+       , words
+       , unwords
        , module Data.Text.Encoding
        , module Data.Text.Encoding.Error
-       , module Text.Read
 
          -- * ByteString
        , ByteString
@@ -31,8 +43,173 @@ module Relude.String.Reexport
 import Data.ByteString (ByteString)
 import Data.ByteString.Short (ShortByteString, fromShort, toShort)
 import Data.String (IsString (..), String)
-import Data.Text (Text, lines, unlines, unwords, words)
+import Data.Text (Text)
 import Data.Text.Encoding (decodeUtf8', decodeUtf8With)
 import Data.Text.Encoding.Error (OnDecodeError, OnError, UnicodeException, lenientDecode,
                                  strictDecode)
+import GHC.TypeLits (ErrorMessage (..), Symbol, TypeError)
 import Text.Read (Read, readMaybe, reads)
+
+import Relude.Base (Constraint, Type)
+
+import qualified Data.Text as Text
+
+
+-- $setup
+-- >>> import Relude
+
+-- | For tracking usage of Text instead of String
+type IsText
+    (t :: Type)      -- Textual type, e.g. Text, String
+    (fun :: Symbol)  -- Function name
+    = (t ~ Text, CheckText t fun)
+
+type family CheckText (t :: Type) (fun :: Symbol) :: Constraint where
+    CheckText Text _ = ()
+    CheckText String fun = TypeError
+        ( 'Text "'" ':<>: 'Text fun ':<>: 'Text "' works with 'Text', not 'String'."
+        ':$$: 'Text "Possible fixes:"
+        ':$$: 'Text "    1. Make sure OverloadedStrings extension is enabled."
+        ':$$: 'Text "    2. Apply 'toText' to a single value."
+        ':$$: 'Text "    3. Apply 'map toText' to the list value."
+        )
+    CheckText a fun = TypeError
+        ( 'Text "'" ':<>: 'Text fun ':<>: 'Text "' works with 'Text'"
+        ':$$: 'Text "But given: '" ':<>: 'ShowType a ':<>: 'Text "'"
+        )
+
+{- | 'lines' takes 'Text' and splits it into the list by lines.
+
+Actual type of this function is the following:
+
+@
+lines :: 'Text' -> ['Text']
+@
+
+but it was given a more complex type to provide friendlier compile time errors.
+
+>>> lines ""
+[]
+>>> lines "one line"
+["one line"]
+>>> lines "line 1\nline 2"
+["line 1","line 2"]
+>>> lines ("string line" :: String)
+...
+... 'lines' works with 'Text', not 'String'.
+      Possible fixes:
+          1. Make sure OverloadedStrings extension is enabled.
+          2. Apply 'toText' to a single value.
+          3. Apply 'map toText' to the list value.
+...
+>>> lines True
+...
+... 'lines' works with 'Text'
+      But given: 'Bool'
+...
+-}
+lines :: IsText t "lines" => t -> [t]
+lines = Text.lines
+{-# INLINE lines #-}
+
+{- | 'unlines' takes list of 'Text' values and joins them with line separator.
+
+Actual type of this function is the following:
+
+@
+unlines :: ['Text'] -> 'Text'
+@
+
+but it was given a more complex type to provide friendlier compile time errors.
+
+>>> unlines []
+""
+>>> unlines ["line 1"]
+"line 1\n"
+>>> unlines ["first line", "second line"]
+"first line\nsecond line\n"
+>>> unlines (["line 1", "line 2"] :: [String])
+...
+... 'unlines' works with 'Text', not 'String'.
+      Possible fixes:
+          1. Make sure OverloadedStrings extension is enabled.
+          2. Apply 'toText' to a single value.
+          3. Apply 'map toText' to the list value.
+...
+>>> unlines [True, False]
+...
+... 'unlines' works with 'Text'
+      But given: 'Bool'
+...
+-}
+unlines :: IsText t "unlines" => [t] -> t
+unlines = Text.unlines
+{-# INLINE unlines #-}
+
+{- | 'words' takes 'Text' and splits it into the list by words.
+
+Actual type of this function is the following:
+
+@
+words :: 'Text' -> ['Text']
+@
+
+but it was given a more complex type to provide friendlier compile time errors.
+
+>>> words ""
+[]
+>>> words "one line"
+["one","line"]
+>>> words "   >_<   "
+[">_<"]
+>>> words ("string words" :: String)
+...
+... 'words' works with 'Text', not 'String'.
+      Possible fixes:
+          1. Make sure OverloadedStrings extension is enabled.
+          2. Apply 'toText' to a single value.
+          3. Apply 'map toText' to the list value.
+...
+>>> words True
+...
+... 'words' works with 'Text'
+      But given: 'Bool'
+...
+-}
+words :: IsText t "words" => t -> [t]
+words = Text.words
+{-# INLINE words #-}
+
+{- | 'unwords' takes list of 'Text' values and joins them with space character.
+
+Actual type of this function is the following:
+
+@
+unwords :: ['Text'] -> 'Text'
+@
+
+but it was given a more complex type to provide friendlier compile time errors.
+
+>>> unwords []
+""
+>>> unwords ["singleWord"]
+"singleWord"
+>>> unwords ["word", "another"]
+"word another"
+>>> unwords (["word", "another"] :: [String])
+...
+... 'unwords' works with 'Text', not 'String'.
+      Possible fixes:
+          1. Make sure OverloadedStrings extension is enabled.
+          2. Apply 'toText' to a single value.
+          3. Apply 'map toText' to the list value.
+...
+>>> unwords [True, False]
+...
+... 'unwords' works with 'Text'
+      But given: 'Bool'
+...
+-}
+unwords :: IsText t "unwords" => [t] -> t
+unwords = Text.unwords
+{-# INLINE unwords #-}
