cdbb56b17b568127311678ba8e83153ad8a91540
Veronika Romashkina <vrom911@gmail.com>
2020-05-13 23:28:59 +0100

    Update src/Relude/List/NonEmpty.hs
    Co-authored-by: hint-man[bot] <44720633+hint-man[bot]@users.noreply.github.com>

--- src/Relude/List/NonEmpty.hs
+++ src/Relude/List/NonEmpty.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE ConstraintKinds      #-}
 {-# LANGUAGE DataKinds            #-}
-{-# LANGUAGE KindSignatures       #-}
 {-# LANGUAGE Safe                 #-}
 {-# LANGUAGE TypeFamilies         #-}
 {-# LANGUAGE TypeOperators        #-}
